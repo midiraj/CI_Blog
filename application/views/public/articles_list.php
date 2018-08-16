306534fba105860813ccd1af746068e5a1e6625c
midiraj <shekh.deep@gmail.com>
2018-08-16 11:04:46 +0600

    Add CSS/JS file

--- application/views/public/articles_list.php
+++ application/views/public/articles_list.php
@@ -11,10 +11,25 @@
 
 </head>
 <body>
-	<div class="container">
-		
-		<h1>Hello Everyone</h1>
-	</div>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	  <a class="navbar-brand" href="#">Articles</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
 
+	  <div class="collapse navbar-collapse" id="navbarColor03">
+	      <form class="form-inline my-2 my-lg-0 ml-2 navbar-left">
+	        <input class="form-control mr-sm-2" type="text" placeholder="Search">
+	        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+	      </form>
+	      <ul class="nav navbar-nav navbar-right">
+		      <li class="nav-item" >
+		        <a class="nav-link" href="#">Login</a>
+		      </li>
+	      </ul>
+	  </div>
+	</nav>
+	<script src="<?= base_url('assets/js/jquery.js') ?>"></script>
+	<script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
 </body>
 </html>
