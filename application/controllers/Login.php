315dec4526c752e25c1c2ec758b9ee97a36df51e
midiraj <shekh.deep@gmail.com>
2018-08-16 13:15:55 +0600

    Login Form Validation

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -8,14 +8,25 @@ class Login extends MY_Controller
 	
 	public function index()
 	{
-		$this->load->helper('form');
 		$this->load->view('public/admin_login');
 	}
 
 
 	public function admin_login()
 	{
-		echo "Reached Admin Login Function";
+		$this->load->library('form_validation');
+
+		$this->form_validation->set_rules('username','User Name', 'required|alpha|trim');
+		$this->form_validation->set_rules('password','Password','required');
+		$this->form_validation->set_error_delimiters("<p class='text-danger'>","</p>");
+
+		if ($this->form_validation->run() == TRUE) {
+			echo "Validation Successful";
+		}else{
+			$this->load->view('public/admin_login');
+			// echo validation_errors();
+		}
+
 	}
 
 }
--- application/views/public/admin_login.php
+++ application/views/public/admin_login.php
@@ -3,15 +3,17 @@
 <div class="container">
 	<?php echo form_open('login/admin_login',['class' => 'form-horizontal']); ; ?>
 	  <fieldset class="mt-5">
-	    <center><legend>Login Form</legend></center>
+	    <center><legend>Admin Login</legend></center>
 	    <div class="form-group">
 	      <label for="username">User Name</label>
 	      <?php echo form_input(['name' => 'username', 'class' => 'form-control','placeholder' => 'username']); ?>
 	    </div>
+	    <span><?php echo form_error('username'); ?> </span>
 	    <div class="form-group">
 	      <label for="password">Password</label>
 	      <?php echo form_password(['name' => 'password', 'class' => 'form-control', 'placeholder' => 'Password' ]); ?>
 	    </div>
+	    <span><?php echo form_error('password'); ?> </span>
 	    <?php echo form_reset(['name' => 'reset', 'value' => 'Reset', 'class' => 'btn btn-secondary']) ?>
 	    <?php echo form_submit(['name' => 'submit', 'value' => 'Login', 'class' => 'btn btn-primary']); ?>
 	  </fieldset>
